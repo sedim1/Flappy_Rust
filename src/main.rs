d4830b0d59b07d28f8557230f8fd1d98e0c9356e
sedim1 <elsedim09@gmail.com>
2025-09-06 22:20:20 -0600

    Collision and pipes movement

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 use raylib::prelude::*;
 use raylib::consts::*;
+use rand::Rng;
 
 //Window sizes
 const SW : f64 = 640.0;
-const SH : f64 = 800.0;
+const SH : f64 = 600.0;
 const SW2 : f64 = SW/2.0;
 const SH2 : f64 = SH/2.0;
 const GRAVITY : f64 = 9.81 * 2.0;
 
+//PIPES consts values
+const PIPE_W : f64 = 90.0;
+const PIPE_H : f64 = 550.0;
+const SPACE_BETWEEN_PIPES : f64 = 200.0;
+const MAX_PIPES : usize = 4;
+
 //Vector2 definition
-#[derive(Debug)]
+#[derive(Clone, Copy)]
 struct Vector2{
     x : f64,
     y : f64
@@ -20,6 +27,33 @@ impl Vector2 {
     fn new(x : f64, y : f64) -> Self{ Self { x, y } }
     fn zero() -> Vector2 { Vector2 { x : 0.0, y : 0.0 } }
 }
+
+//Collision shapes
+#[derive(Clone, Copy)]
+struct Rect {
+    position : Vector2,
+    width : f64,
+    height : f64,
+}
+
+impl Rect {
+
+    fn new(position : Vector2,width : f64, height : f64) -> Self{
+        Rect { position, width, height }
+    }
+
+    fn draw(&self,d : &mut RaylibDrawHandle){
+        d.draw_rectangle(self.position.x as i32, self.position.y as i32, self.width as i32, self.height as i32, Color::BLUE);
+    }
+
+    fn intersects_aabb(A : &Rect, B : &Rect) -> bool {
+        let collisionX = A.position.x + A.width >= B.position.x && B.position.x + B.width >= A.position.x;
+        let collisionY = A.position.y + A.height >= B.position.y && B.position.y + B.height >= A.position.y;
+        collisionX && collisionY
+    }
+}
+
+
 
 /*Player definition*/
 struct Player{
@@ -60,6 +94,93 @@ impl Player{
 
 }
 
+//Pipe managers
+#[derive(Clone,Copy)]
+struct Pipe{
+    top : Rect,
+    bottom : Rect,
+}
+
+impl Pipe{
+
+    fn new(top : Rect, bottom : Rect) -> Self {
+        Pipe{
+            top,
+            bottom
+        }
+    }
+
+    fn out_of_bounds(&self) -> bool {
+        self.top.position.x <= - PIPE_W - 10.0
+    }
+
+    fn reset_pipe_x(&mut self){
+        let posx : f64 = SW + (PIPE_W * (MAX_PIPES as f64 + 1.5) ) ;
+        self.top.position.x = posx;
+        self.bottom.position.x = posx;
+    }
+
+    fn reset_pipe_y(&mut self){
+        let pivot : f64 = ( rand::thread_rng().gen_range(70..SH as i32 -70) ) as f64;
+        self.top.position.y = pivot - (SPACE_BETWEEN_PIPES*0.5) - PIPE_H;
+        self.bottom.position.y = pivot + (SPACE_BETWEEN_PIPES*0.5);
+    }
+}
+
+struct PipeManager{
+    pipes : [Pipe;MAX_PIPES]
+}
+
+impl PipeManager{
+    fn new() -> Self {
+        let rect1 : Rect = Rect::new(Vector2::zero(),PIPE_W,PIPE_H);
+        let rect2 : Rect = Rect::new(Vector2::zero(),PIPE_W,PIPE_H);
+        let pipe : Pipe = Pipe::new(rect1,rect2);
+        PipeManager { pipes : [ pipe ; MAX_PIPES] }
+    }
+
+    fn reset(&mut self){
+        for i in 0..MAX_PIPES {
+            let dist_x : f64 = PIPE_W * 3.5;
+            let start_pos_x : f64 = i as f64 * dist_x;
+            self.pipes[i].reset_pipe_y();
+            self.pipes[i].top.position.x = SW + start_pos_x;
+            self.pipes[i].bottom.position.x = SW + start_pos_x;
+        }
+    }
+
+    fn player_collision_pipes(&self,player : &Player) -> bool{
+        let player_rect : Rect = Rect::new(Vector2::new(player.position.x - 30.0,player.position.y),30.0,30.0);
+        for i in 0..MAX_PIPES{
+            if Rect::intersects_aabb(&player_rect,&self.pipes[i].top) ||Rect::intersects_aabb(&player_rect, &self.pipes[i].bottom) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    fn update(&mut self, dt : f64) {
+        let speed_movement : f64 = 200.0;
+        for i in 0..MAX_PIPES {
+            self.pipes[i].top.position.x -= speed_movement * dt;
+            self.pipes[i].bottom.position.x -= speed_movement * dt;
+            if self.pipes[i].out_of_bounds() {
+                self.pipes[i].reset_pipe_x();
+                self.pipes[i].reset_pipe_y();
+            }
+        }
+
+    }
+
+    fn render(&self, d : &mut RaylibDrawHandle){
+        for i in 0..MAX_PIPES {
+            self.pipes[i].top.draw(d);
+            self.pipes[i].bottom.draw(d);
+        }
+    }
+}
+
+
 
 fn main() {
     //Init raylib and setup window
@@ -75,22 +196,25 @@ fn main() {
 }
 
 fn main_loop(rl : &mut RaylibHandle, thread : &RaylibThread) {
-    let mut player = Player::new();
-
+    let mut player : Player = Player::new();
+    let mut pipes : PipeManager = PipeManager::new();
+    pipes.reset();
     while !rl.window_should_close() {
         let delta_time : f64 = rl.get_frame_time() as f64;
-        update(rl,delta_time,&mut player);
-        render(rl,thread,&player);
+        update(rl,delta_time,&mut player,&mut pipes);
+        render(rl,thread,&player,&pipes);
     }
 }
 
-fn update(rl : &mut RaylibHandle, dt : f64 ,player : &mut Player){
+fn update(rl : &mut RaylibHandle, dt : f64 ,player : &mut Player,pipes : &mut PipeManager){
     player.update(rl,dt);
+    pipes.update(dt);
 }
 
-fn render(rl : &mut RaylibHandle,thread : &RaylibThread,player : &Player){
+fn render(rl : &mut RaylibHandle,thread : &RaylibThread,player : &Player, pipes : &PipeManager){
     let mut d : RaylibDrawHandle = rl.begin_drawing(thread);//Lets us acces drawing calls
     d.clear_background( Color::WHITE);
+    pipes.render(&mut d);
     player.render(&mut d);
 }
 
